3f4a651fb2e67b36f6202a3dde963b22fae0150f
Prashancoder <ps985094@gmail.com>
2026-02-26 12:11:54 +0530

    eweew

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -364,8 +364,8 @@ function Nav() {
     };
 
     return (
-        <nav className='w-full fixed top-0 bg-black/80 backdrop-blur-sm z-50 shadow-lg'>
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+<nav className='w-full bg-black shadow-lg'>
+                <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
                 <div className='flex justify-between items-center h-20'>
 
                     {/* Logo */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,16 +28,12 @@ function Home() {
     
     
     <div className='w-[100%] overflow-hidden'>
-      
-      <div className='w-[100%] lg:h-[120vh] h-[70vh] relative'>
-        <Nav/>
+
+     <Nav/>
+
+      <div className='w-[100%] lg:h-[90vh] h-[60vh] relative'>
         <img src={home} className='object-cover md:object-fill   w-[100%] lg:h-[100%] h-[50vh]' alt="" />
-        <span className='lg:text-[70px] absolute  md:text-[40px]  lg:top-[10%] top-[15%] w-[100%] flex items-center justify-center text-white font-bold text-[20px] '>
-        Timeless Aesthetics 
-        </span>
-        <span className='lg:text-[70px] text-[20px] md:text-[40px] absolute lg:top-[18%] top-[20%] w-[100%] flex items-center justify-center text-white font-bold'>
-          Your Career path
-        </span>
+ 
         <div className='absolute lg:top-[30%] top-[75%]  md:top-[80%] w-[100%] flex items-center justify-center gap-3 flex-wrap'>
           
       {/* <button className='px-[20px] py-[10px] lg:bg-white bg-black lg:text-black text-white rounded-[10px] text-[18px] font-light flex gap-2 cursor-pointer items-center justify-center' onClick={()=>navigate("/searchwithai")}>Search with AI <img src={ai} className='w-[30px] h-[30px] rounded-full hidden lg:block' alt="" /><img src={ai1} className='w-[35px] h-[35px] rounded-full lg:hidden' alt="" /></button> */}
