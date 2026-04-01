5ec59504453fe337edae5475db2f5b005caa91d0
Prashancoder <ps985094@gmail.com>
2026-04-01 16:06:25 +0530

    ds

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -506,7 +506,7 @@ function Nav() {
 
             {/* Mobile Sidebar/Drawer Menu */}
 <div
-  className={`fixed top-0 right-0 w-[80vw] sm:w-[60vw] md:w-[50vw] max-w-sm min-h-screen bg-gray-900 overflow-y-auto transform transition-transform duration-300 ease-in-out z-[100] ${
+  className={`fixed top-0 right-0 w-[80vw] sm:w-[60vw] md:w-[50vw] max-w-sm min-h-screen bg-black overflow-y-auto transform transition-transform duration-300 ease-in-out z-[100] ${
     showHam ? "translate-x-0" : "translate-x-full"
   }`}
 >                
